5d6efd01d22193f8f0c6edda053b8e3980ed9871
maybeerfan <maybeerfan@gmail.com>
2025-05-07 13:43:01 +0000

    start

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h2> Login form </h2>
+    <form action="login.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username" required><br>
 
-?>
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" required><br>
+    <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
